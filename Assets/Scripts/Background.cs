481e8f532d467057dab287ad12333059bd5492b0
v <v@vs-iMac.local>
2015-08-26 18:31:36 -0400

    scrolling background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	float pos = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update () {
+		pos += .004f;
+		
+		
+		Transform quad = transform.Find ("Gravel");
+		quad.renderer.material.mainTextureOffset = new Vector2(0, pos);
+	}
+}
