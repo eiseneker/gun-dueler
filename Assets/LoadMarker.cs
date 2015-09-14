1dbb15c3f18966262f4d9dd24ecf6c8e727352bf
v <v@vs-iMac.local>
2015-09-14 16:23:21 -0400

    road generated procedurally

---
+++ Assets/LoadMarker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMarker : MonoBehaviour {
+
+	public Road road;
+	
+	private int hitCount = 0;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if(collider.GetComponent<Player>()){
+			hitCount++;
+			if(hitCount == 1){
+				road.LoadNewRoad();
+			}else{
+				road.UnloadFirstRoad();			
+			}		
+		}
+	}
+}
---
+++ Assets/Road.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Road : MonoBehaviour {
+
+	private LoadMarker loadMarker;
+	
+	private static ArrayList roads = new ArrayList();
+
+	// Use this for initialization
+	void Start () {
+		loadMarker = transform.Find ("LoadMarker").GetComponent<LoadMarker>();
+		loadMarker.road = this;
+		roads.Add (this);
+	}
+	
+	
+	public void LoadNewRoad(){
+		GameObject road = Instantiate (gameObject, new Vector3(transform.position.x + 50, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+		road.transform.parent = transform.parent;
+	}
+	
+	public void UnloadFirstRoad(){
+		Road road = roads[0] as Road;
+		if(road != this){
+			roads.Remove(road);
+			Destroy (road.gameObject);
+		}
+	}
+}
